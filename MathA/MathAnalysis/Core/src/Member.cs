c81537424f4b96a0e12bd02441fa6485a08f3062
Sami Daniel <samidaniel125@gmail.com>
2024-02-24 12:41:00 -0300

    added member prop MemberKind enum to classify the Member

--- MathA/MathAnalysis/Core/src/Member.cs
+++ MathA/MathAnalysis/Core/src/Member.cs
@@ -1,9 +1,14 @@
-﻿namespace MathA.MathAnalysis.Core.src
+﻿using MathA.MathAnalysis.Core.src.Enums;
+
+namespace MathA.MathAnalysis.Core.src
 {
     internal struct Member
     {
+        private MemberKind kind;
+
         public readonly double Coefficient { get; }
         public readonly string CompValue { get; }
         public readonly string? Variable { get; }
+        public MemberKind Kind { readonly get => kind; set => kind = value; }
     }
 }
