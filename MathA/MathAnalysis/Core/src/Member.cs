a9db20ccc23aa689a003f97dad1feb0c0f3f384e
Sami Daniel <samidaniel125@gmail.com>
2024-02-24 14:47:05 -0300

    created ctor to member class

--- MathA/MathAnalysis/Core/src/Member.cs
+++ MathA/MathAnalysis/Core/src/Member.cs
@@ -10,5 +10,13 @@ namespace MathA.MathAnalysis.Core.src
         public readonly string CompValue { get; }
         public readonly string? Variable { get; }
         public MemberKind Kind { readonly get => kind; set => kind = value; }
+
+        public Member(MemberKind _kind, double coefficient, string compValue, string? variable) : this()
+        {
+            Kind = _kind;
+            Coefficient = coefficient;
+            CompValue = compValue ?? throw new ArgumentNullException(nameof(compValue));
+            Variable = variable;
+        }
     }
 }
