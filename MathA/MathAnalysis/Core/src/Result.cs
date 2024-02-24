9d3e670f7d2e9805f70a666e679a45b2237b5fe3
Sami Daniel <samidaniel125@gmail.com>
2024-02-24 16:03:16 -0300

    added Result struct to store result from math

---
+++ MathA/MathAnalysis/Core/src/Result.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MathA.MathAnalysis.Core.src
+{
+    internal readonly struct Result(string val)
+    {
+        readonly string val = val ?? throw new ArgumentNullException(nameof(val));
+
+        public override string ToString() => val;
+    }
+}
