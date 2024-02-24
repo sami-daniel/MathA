a4cbc408ec0e1c1ac96147b12f6fcc78e17bfa58
Sami Daniel <samidaniel125@gmail.com>
2024-02-24 16:09:52 -0300

    creation of the Statics structure containing MathB class with basic mathematical methods

---
+++ MathA/MathAnalysis/Core/src/Statics/MathB.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace MathA.MathAnalysis.Core.src.Statics
+{
+    internal static class MathB
+    {
+        public static double Evaluate(string expression)
+        {
+            DataTable table = new();
+            table.Columns.Add("expression", string.Empty.GetType(), expression);
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+            return double.Parse((string)row["expression"]);
+        }
+    }
+}
