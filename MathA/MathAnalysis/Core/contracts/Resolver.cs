104623fa2e48d45f1f8ed5e3e0d130af135ea8c9
Sami Daniel <samidaniel125@gmail.com>
2024-02-24 16:16:30 -0300

    Creation of the Contracts structure that will contain contracts. First Contract Created ISolver

---
+++ MathA/MathAnalysis/Core/contracts/Resolver.cs
@@ -0,0 +1,10 @@
+﻿using MathA.MathAnalysis.Core.src;
+
+namespace MathA.MathAnalysis.Core.contracts
+{
+    internal interface Resolver
+    {
+        Result Result { get; }
+        void Resolve();
+    }
+}
