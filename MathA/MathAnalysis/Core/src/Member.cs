e2ead6835477504fd712ea424d79b76c3befd1f3
Sami Daniel <samidaniel125@gmail.com>
2024-02-24 17:54:54 -0300

    Modify readonly property and class, add ConfigV method, and implemented ISolver interface

--- MathA/MathAnalysis/Core/src/Member.cs
+++ MathA/MathAnalysis/Core/src/Member.cs
@@ -1,20 +1,78 @@
-﻿using MathA.MathAnalysis.Core.src.Enums;
+﻿using MathA.MathAnalysis.Core.contracts;
+using MathA.MathAnalysis.Core.src.Enums;
+using MathA.MathAnalysis.Core.src.Statics;
 
 namespace MathA.MathAnalysis.Core.src
 {
-    internal readonly struct Member
+    internal struct Member : ISolver
     {
-        public MemberKind Kind { get; }
-        public double Coefficient { get; }
-        public string CompValue { get; }
-        public string? Variable { get; }
+        public readonly MemberKind Kind { get; }
+        public double Coefficient { get; private set; }
+        public readonly string CompValue { get; }
+        public string? Variable { get; private set; }
+        public Result? Result { get; private set; }
 
-        public Member(MemberKind _kind, double coefficient, string compValue, string? variable) : this()
+        public Member(MemberKind _kind, string compValue) : this()
         {
             Kind = _kind;
-            Coefficient = coefficient;
             CompValue = compValue ?? throw new ArgumentNullException(nameof(compValue));
-            Variable = variable;
+            try
+            {
+                this.SolveR();
+            }
+            catch
+            {
+                ConfigV();
+            }
+        }
+
+        void ConfigV()
+        {
+            var coe = "";
+            foreach (var c in CompValue)
+            {
+                if (char.IsDigit(c))
+                {
+                    coe += c;
+                }
+                else if (char.IsLetter(c))
+                {
+                    if (Variable != c.ToString())
+                    {
+
+                    }
+                    else
+                    {
+                        Variable = c.ToString();
+                    }
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        void ISolver.Solve()
+        {
+            SolveR();
+        }
+        void SolveR()
+        {
+            try
+            {
+                Result = new(MathB.Evaluate(CompValue).ToString());
+                Coefficient = MathB.Evaluate(CompValue);
+            }
+            catch
+            {
+                Result = new(CompValue);
+            }
+        }
+
+        public override string ToString()
+        {
+            return CompValue + ", kind of" + Kind.ToString();
         }
     }
 }
