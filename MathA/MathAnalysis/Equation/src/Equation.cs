b41a3e1da1bb4177b8cf55d4eb304ddc4111b248
Sami Daniel <samidaniel125@gmail.com>
2024-02-25 10:25:11 -0300

    added new ctor Equation

--- MathA/MathAnalysis/Equation/src/Equation.cs
+++ MathA/MathAnalysis/Equation/src/Equation.cs
@@ -1,15 +1,28 @@
 ﻿using MathA.MathAnalysis.Core.contracts;
 using MathA.MathAnalysis.Core.src;
+using MathA.MathAnalysis.Core.src.Enums;
 
 namespace MathA.MathAnalysis.Equation.src
 {
-    internal struct Equation
+    internal struct Equation : ISolver
     {
-        List<Member> LeftSideMembers = new List<Member>();
-        List<Member> RightSideMember = new List<Member>();
+        List<Member> LeftSideMembers = [];
+        List<Member> RightSideMember = [];
         public Member LeftSide { get; private set; }
         public Member RightSide { get; private set;}
         public readonly string Variable { get; }
-        
+
+        public Result? Result => throw new NotImplementedException();
+
+        public Equation(string equation) : this()
+        {
+            
+        }
+
+        public void Solve()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
