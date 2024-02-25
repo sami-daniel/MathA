40a3061964fb0df94813be006a378890f6561dfc
Sami Daniel <samidaniel125@gmail.com>
2024-02-25 09:44:52 -0300

    insert of initial props and fields in Equation struct

--- MathA/MathAnalysis/Equation/src/Equation.cs
+++ MathA/MathAnalysis/Equation/src/Equation.cs
@@ -1,7 +1,15 @@
-﻿namespace MathA.MathAnalysis.Equation.src
+﻿using MathA.MathAnalysis.Core.contracts;
+using MathA.MathAnalysis.Core.src;
+
+namespace MathA.MathAnalysis.Equation.src
 {
     internal struct Equation
     {
-
+        List<Member> LeftSideMembers = new List<Member>();
+        List<Member> RightSideMember = new List<Member>();
+        public Member LeftSide { get; private set; }
+        public Member RightSide { get; private set;}
+        public readonly string Variable { get; }
+        
     }
 }
