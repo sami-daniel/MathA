8a7aec27b1058dd346471cd242b27fd938fa66c7
Sami Daniel <samidaniel125@gmail.com>
2024-02-25 11:56:34 -0300

    Create spliteqtmember and block Formateqt, try-catch block

--- MathA/MathAnalysis/Equation/src/Equation.cs
+++ MathA/MathAnalysis/Equation/src/Equation.cs
@@ -11,23 +11,44 @@ namespace MathA.MathAnalysis.Equation.src
         List<Member> RightSideMember = [];
         public Member LeftSide { get; private set; }
         public Member RightSide { get; private set;}
-        public readonly string Variable { get; }
+        public string Variable { get; private set; } = "";
 
         public Result? Result => throw new NotImplementedException();
 
         public Equation(string equation) : this()
         {
-            equation = FormatEqt(equation);
-            LeftSide = new(MemberKind.Side, equation.Split('=')[0]);
-            RightSide = new(MemberKind.Side, equation.Split('=')[1]);
+            try
+            {
+                equation = FormatEqt(equation);
+                LeftSide = new(MemberKind.Side, equation.Split('=')[0]);
+                RightSide = new(MemberKind.Side, equation.Split('=')[1]);
+                SplitEqtMember();
+            }
+            catch
+            {
+
+            }
         }
 
-        public void Solve()
+        void ISolver.Solve()
         {
             throw new NotImplementedException();
         }
 
-        string FormatEqt(string eqt) => Regex.Replace(eqt, "\\s+", " ");
+        private static string FormatEqt(string eqt)
+        {
+            return Regex.Replace(eqt, "\\s+", " ");
+        }
 
+        readonly void SplitEqtMember()
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            for (int i = 0; i < LeftSide.Result.ToString().Split(' ').Length; i++)
+            {
+                LeftSideMembers.Add(new Member(MemberKind.Term, LeftSide.Result.ToString().Split(' ')[i]));
+            }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            
+        }
     }
 }
